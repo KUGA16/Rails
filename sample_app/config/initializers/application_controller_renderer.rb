097547c5f042bcd6776d0c7ff39bba5b921c02f1
KUGA <gachan_1216@yahoo.co.jp>
2019-12-25 23:33:00 +0900

    [Finish] 3-10

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0a5f77453c6360bd6597057a99c4d5de5e3d865adec52da59ebe9b1c7a03d3bebfa72d70a3f0300dba7fc9c82cc3a7bd56233a9c9b1148e8d7a6cb51ce3bf63a'
